c66d8b0bd2e3b8c1fb536049b865a190364d0750
[Grigorii] <[glisinenko@rzd.dev]>
2026-06-03 15:00:14 +0300

    Add /time command

--- bot.py
+++ bot.py
@@ -1,5 +1,7 @@
 import telebot
+from telebot import types
 from config import ALLOWED_USER_IDs, TELEGRAM_TOKEN
+from datetime import datetime
 
 bot = telebot.TeleBot(TELEGRAM_TOKEN)
 
@@ -18,6 +20,20 @@ def test(message):
         return
     bot.send_message(message.chat.id, "test message")
 
+
+@bot.message_handler(commands=["time"])
+def test(message):
+    if not is_allowed:
+        return
+    now = datetime.now()
+    time = f"Московское время: {now.hour} часов, {now.minute} минут."
+    bot.send_message(message.chat.id, time)
+
+
+"""
+def main_keyboard():
+    markup = types.KeyboardButton("")
+"""
 
 if __name__ == "__main__":
     print("Бот запущен")
