b0214c6cc909b7b01a5c4f62e60187d85c3b40b7
[Grigorii] <[glisinenko@rzd.dev]>
2026-06-04 16:41:11 +0300

    Disks update after problem with saving

--- bot.py
+++ bot.py
@@ -53,13 +53,85 @@ def server(message):
     else:
         bot.send_message(message.chat.id, "Нет информации")
 
+    """
+                                    # Возврат часов
+    now = datetime.now()
+    bot.send_chat_action(message.chat.id, "typing")
+    time = f"Московское время: {now.hour} часов, {now.minute} минут."
+    bot.send_message(message.chat.id, time)
+    """
+
 
 @bot.message_handler(func=lambda message: message.text == "Диски")
 @restricted
 def disks(message):
     bot.send_chat_action(message.chat.id, "typing")
-    time.sleep(5)
-    bot.send_message(message.chat.id, "disks info")
+
+    root_free = 'node_filesystem_avail_bytes{mountpoint="/"} / 1024 / 1024 / 1024'
+    root_total = 'node_filesystem_size_bytes{mountpoint="/"} / 1024 / 1024 / 1024'
+    docker_free = (
+        'node_filesystem_avail_bytes{mountpoint="/var/lib/docker"} / 1024 / 1024 / 1024'
+    )
+    docker_total = (
+        'node_filesystem_size_bytes{mountpoint="/var/lib/docker"} / 1024 / 1024 / 1024'
+    )
+    hdd_toshiba_free = 'node_filesystem_avail_bytes{mountpoint="/media/toshiba_650"} / 1024 / 1024 / 1024'
+    hdd_toshiba_total = 'node_filesystem_size_bytes{mountpoint="/media/toshiba_650"} / 1024 / 1024 / 1024'
+
+    root_free = get_prometheus_metric(root_free)
+    root_total = get_prometheus_metric(root_total)
+    docker_free = get_prometheus_metric(docker_free)
+    docker_total = get_prometheus_metric(docker_total)
+    hdd_toshiba_free = get_prometheus_metric(hdd_toshiba_free)
+    hdd_toshiba_total = get_prometheus_metric(hdd_toshiba_total)
+
+    root_used = (
+        root_total - root_free
+        if (root_total is not None and root_free is not None)
+        else None
+    )
+    docker_used = (
+        docker_total - docker_free
+        if (docker_total is not None and docker_free is not None)
+        else None
+    )
+    hdd_toshiba_used = (
+        hdd_toshiba_total - hdd_toshiba_free
+        if (hdd_toshiba_total is not None and hdd_toshiba_free is not None)
+        else None
+    )
+
+    def format_string(name, used, free, total):
+        a = []
+
+        if used is not None:
+            a.append(f"использовано {used:.1f} Gb")
+        else:
+            a.append(f"использовано НЕТ ДАННЫХ")
+
+        if free is not None:
+            a.append(f"свободно {free:.1f} Gb")
+        else:
+            a.append(f"свободно НЕТ ДАННЫХ")
+
+        if total is not None:
+            a.append(f"всего {total:.1f} Gb")
+        else:
+            a.append(f"всего НЕТ ДАННЫХ")
+
+        string = f"{name}: " + ", ".join(a) + "\n"
+
+        return string
+
+    total = "Статус дисков:\n\nSSD:\n"
+    total += format_string("Root", root_used, root_free, root_total)
+    total += format_string("Docker", docker_used, docker_free, docker_total)
+    total += "\nHDD:\n"
+    total += format_string(
+        "Toshiba 650", hdd_toshiba_used, hdd_toshiba_free, hdd_toshiba_total
+    )
+
+    bot.send_message(message.chat.id, total)
 
 
 def main_keyboard():
