e689005b336a48ccaa7de3cb387cf1d16b0e9223
[Grigorii] <[glisinenko@rzd.dev]>
2026-06-16 13:54:25 +0300

    Total update, add Classes

--- bot.py
+++ bot.py
@@ -18,139 +18,6 @@ def restricted(func):
     return wrapper
 
 
-def get_prometheus_metric(query):
-    try:
-        response = requests.get(
-            "http://192.168.1.45:9090/api/v1/query", params={"query": query}, timeout=5
-        )
-        if response.status_code == 200:
-            data = response.json()
-            if data["data"]["result"]:
-                value = data["data"]["result"][0]["value"][1]
-                return float(value)
-    except:
-        pass
-
-
-@bot.message_handler(func=lambda message: message.text == "ИБП")
-@restricted
-def ups(message):
-    bot.send_chat_action(message.chat.id, "typing")
-    time.sleep(10)
-    bot.send_message(message.chat.id, "test message")
-
-
-@bot.message_handler(func=lambda message: message.text == "Сервер")
-@restricted
-def server(message):
-    bot.send_chat_action(message.chat.id, "typing")
-
-    cpu_query = '100 - (avg by (instance) (rate(node_cpu_seconds_total{mode="idle"}[5m])) * 100)'
-    cpu_temp_query = (
-        'node_hwmon_temp_celsius{chip="platform_coretemp_0", sensor="temp1"}'
-    )
-    ram_query = (
-        "(1 - (node_memory_MemAvailable_bytes / node_memory_MemTotal_bytes)) * 100"
-    )
-
-    cpu = get_prometheus_metric(cpu_query)
-    cpu_temp = get_prometheus_metric(cpu_temp_query)
-    ram = get_prometheus_metric(ram_query)
-
-    total = "Статус сервера: \n\n"
-    total += f"CPU: <b>{cpu:.1f}</b> %\n" if cpu is not None else "CPU: нет данных\n"
-    total += (
-        f"CPU, t: <b>{cpu_temp:.1f}</b> °C\n"
-        if cpu_temp is not None
-        else "CPU, t: нет данных\n"
-    )
-    total += f"RAM: <b>{ram:.1f}</b> %\n" if ram is not None else "RAM: нет данных\n"
-
-    bot.send_message(message.chat.id, total, parse_mode="HTML")
-
-
-@bot.message_handler(func=lambda message: message.text == "Диски")
-@restricted
-def disks(message):
-    bot.send_chat_action(message.chat.id, "typing")
-
-    root_free = 'node_filesystem_avail_bytes{mountpoint="/"} / 1024 / 1024 / 1024'
-    root_total = 'node_filesystem_size_bytes{mountpoint="/"} / 1024 / 1024 / 1024'
-    docker_free = (
-        'node_filesystem_avail_bytes{mountpoint="/var/lib/docker"} / 1024 / 1024 / 1024'
-    )
-    docker_total = (
-        'node_filesystem_size_bytes{mountpoint="/var/lib/docker"} / 1024 / 1024 / 1024'
-    )
-    hdd_toshiba_free = 'node_filesystem_avail_bytes{mountpoint="/media/toshiba_650"} / 1024 / 1024 / 1024'
-    hdd_toshiba_total = 'node_filesystem_size_bytes{mountpoint="/media/toshiba_650"} / 1024 / 1024 / 1024'
-    hdd_wd_6tb_free = 'node_filesystem_avail_bytes{mountpoint="/media/wd_6tb"} / 1024 / 1024 / 1024'
-    hdd_wd_6tb_total = 'node_filesystem_size_bytes{mountpoint="/media/wd_6tb"} / 1024 / 1024 / 1024'
-
-    root_free = get_prometheus_metric(root_free)
-    root_total = get_prometheus_metric(root_total)
-    docker_free = get_prometheus_metric(docker_free)
-    docker_total = get_prometheus_metric(docker_total)
-    hdd_toshiba_free = get_prometheus_metric(hdd_toshiba_free)
-    hdd_toshiba_total = get_prometheus_metric(hdd_toshiba_total)
-    hdd_wd_6tb_free = get_prometheus_metric(hdd_wd_6tb_free)
-    hdd_wd_6tb_total = get_prometheus_metric(hdd_wd_6tb_total)
-
-    root_used = (
-        root_total - root_free
-        if (root_total is not None and root_free is not None)
-        else None
-    )
-    docker_used = (
-        docker_total - docker_free
-        if (docker_total is not None and docker_free is not None)
-        else None
-    )
-    hdd_toshiba_used = (
-        hdd_toshiba_total - hdd_toshiba_free
-        if (hdd_toshiba_total is not None and hdd_toshiba_free is not None)
-        else None
-    )
-    hdd_wd_6tb_used = (
-        hdd_wd_6tb_total - hdd_wd_6tb_free
-        if (hdd_wd_6tb_total is not None and hdd_wd_6tb_free is not None)
-        else None
-    )
-
-    def format_string(name, used, free, total):
-        a = []
-
-        if used is not None:
-            a.append(f"использовано <b>{used:.1f}</b> Gb")
-        else:
-            a.append(f"использовано <b>НЕТ ДАННЫХ</b>")
-
-        if free is not None:
-            a.append(f"свободно <b>{free:.1f}</b> Gb")
-        else:
-            a.append(f"свободно <b>НЕТ ДАННЫХ</b>")
-
-        if total is not None:
-            a.append(f"всего <b>{total:.1f}</b> Gb")
-        else:
-            a.append(f"всего <b>НЕТ ДАННЫХ</b>")
-
-        string = f"<b>{name}</b>: " + ", ".join(a) + "\n"
-
-        return string
-
-    total = "<b>Статус дисков</b>:\n\n<b>SSD</b>:\n"
-    total += format_string("Root", root_used, root_free, root_total)
-    total += format_string("Docker", docker_used, docker_free, docker_total)
-    total += "\n<b>HDD</b>:\n"
-    total += format_string(
-        "Toshiba 650 Gb", hdd_toshiba_used, hdd_toshiba_free, hdd_toshiba_total
-    )
-    total += format_string("WD 6 Tb", hdd_wd_6tb_used, hdd_wd_6tb_free, hdd_wd_6tb_total)
-
-    bot.send_message(message.chat.id, total, parse_mode="HTML")
-
-
 def main_keyboard():
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=False)
     button1 = types.KeyboardButton("Сервер")
@@ -165,6 +32,132 @@ def main_keyboard():
 @restricted
 def start(message):
     bot.send_message(message.chat.id, "Выберите действие", reply_markup=main_keyboard())
+
+
+def get_prometheus_metric(query):
+    try:
+        response = requests.get(
+            "http://192.168.1.45:9090/api/v1/query", params={"query": query}, timeout=5
+        )
+        if response.status_code == 200:
+            data = response.json()
+            if data["data"]["result"]:
+                value = data["data"]["result"][0]["value"][1]
+                return float(value)
+    except:
+        pass
+
+
+class Disk:
+    def __init__(self, name, mountpoint):
+        self.name = name
+        self.mountpoint = mountpoint
+
+    def space(self):
+        free = get_prometheus_metric(f'node_filesystem_avail_bytes{{mountpoint="{self.mountpoint}"}}')
+        total = get_prometheus_metric(f'node_filesystem_size_bytes{{mountpoint="{self.mountpoint}"}}')
+        used = total - free if free and total else None
+        return free, total, used
+
+    def format_size(self, byte_values):
+        if byte_values is None:
+            return "нет данных"
+            
+        units = ['B', "KB", "MB", "GB", "TB"]
+        for i in units:
+            if byte_values < 1024:
+                return f"{byte_values:.1f} {i}"
+            byte_values /= 1024
+
+    def format(self):
+        free, total, used = self.space()
+        free = self.format_size(free)
+        total = self.format_size(total)
+        used = self.format_size(used)
+
+        return(f"{self.name}: свободно {free}, использовано {used}, всего {total}\n")
+
+
+
+class Server:
+    def __init__(self, name):
+        self.name = name
+    
+    def get_cpu(self):
+        self.cpu_query = get_prometheus_metric('100 - (avg by (instance) (rate(node_cpu_seconds_total{mode="idle"}[5m])) * 100)')
+        self.cpu_query = self.cpu_query if self.cpu_query is not None else "нет данных"
+        self.cpu_temp_query = get_prometheus_metric(
+        'node_hwmon_temp_celsius{chip="platform_coretemp_0", sensor="temp1"}'
+        )
+        self.cpu_temp_query = self.cpu_temp_query if self.cpu_temp_query is not None else "нет данных"
+        return f"CPU: {self.cpu_query} %\nCPU, t: {self.cpu_temp_query} °C\n"
+
+    def get_ram(self):
+        self.ram_query = get_prometheus_metric(
+        "(1 - (node_memory_MemAvailable_bytes / node_memory_MemTotal_bytes)) * 100"
+        )
+        self.ram_query = self.ram_query if self.ram_query is not None else "нет данных"
+        return f"RAM: {self.ram_query} %"
+    
+    def format(self):
+        return self.get_cpu() + self.get_ram()
+
+
+@bot.message_handler(func=lambda message: message.text == "Сервер")
+@restricted
+def server(message):
+    bot.send_chat_action(message.chat.id, "typing")
+
+    server_1 = Server("MyNAS")
+
+    bot.send_message(message.chat.id, server_1.format(), parse_mode="HTML")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+@bot.message_handler(func=lambda message: message.text == "Диски")
+@restricted
+def disks(message):
+    bot.send_chat_action(message.chat.id, "typing")
+
+#SSD
+    root = Disk('Root', '/')
+    docker = Disk('Docker', '/var/lib/docker')
+
+#HDD
+    toshiba_650 = Disk("Toshiba 650 Gb", '/media/toshiba_650')
+    wd_6tb = Disk('WD 6 Tb', "/media/wd_6tb")
+
+    total = "Статус дисков: \n\nSSD:\n"
+    total += root.format()
+    total += docker.format()
+    total += "\n\nHDD:\n"
+    total += toshiba_650.format()
+    total += wd_6tb.format()
+
+    bot.send_message(message.chat.id, total, parse_mode="HTML")
+
+
+
+
+
+
+
 
 
 if __name__ == "__main__":
